55b4ec5bae2ffb5e5e64b7f039546768d100bd9e
ELLIOTTCABLE <me@ell.io>
2016-10-07 18:31:07 -0500

    + more simple tests for traps()

--- Makefile
+++ Makefile
@@ -168,7 +168,7 @@ UPROGS=\
 	_usertests\
 	_wc\
 	_zombie\
-	_zz_traps_1 _zz_traps_2\
+	_zz_traps_1 _zz_traps_2 _zz_traps_3 _zz_traps_4\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
---
+++ zz_traps_4.c
@@ -0,0 +1,23 @@
+#include "types.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int t, u;
+
+  t = traps();
+
+  printf(0, "initial: %d\n", t);
+  t = traps();
+
+  printf(0, "after printf(): %d\n", t);
+
+  t = traps();
+  uptime();
+  u = traps();
+
+  printf(0, "before / after uptime(): %d/%d\n", t, u);
+
+  exit();
+}
